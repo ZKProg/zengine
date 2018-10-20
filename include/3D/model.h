8895b355b61a45b3182e1be068730e2969437177
Zedka29 <karim.zerf@zkprog.com>
2018-10-20 22:33:19 +0200

    Model.h added.

---
+++ include/3D/model.h
@@ -0,0 +1,27 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "mesh.h"
+
+#include <assimp/mesh.h>
+#include <assimp/scene.h>
+#include <assimp/camera.h>
+#include <assimp/postprocess.h>
+#include <assimp/Importer.hpp>
+
+class Model : public Mesh {
+
+public:
+    Model();
+    virtual ~Model();
+
+protected:
+
+    const aiScene *_scene;
+    std::vector<Mesh> _meshes;
+    Assimp::Importer _importer;
+
+
+};
+
+#endif MODEL_H
